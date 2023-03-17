3778147db130c17cc4adb09879ccbb6659612bf8
hjh1161514 <hjh1161514@naver.com>
2023-03-17 13:29:55 +0900

    [CHORE] 프로젝트 세팅

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_management/screen/state_provider_screen.dart';
+
+import '../layout/default_layout.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+        title: 'HomeScreen',
+        body: ListView(
+          children: [
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => StateProviderScreen()),
+                  );
+                },
+                child: Text('StateProviderScreen'),
+            )
+          ],
+        )
+    );
+  }
+}
